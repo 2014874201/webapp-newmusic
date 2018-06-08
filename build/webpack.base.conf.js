29e28d82e11113fbf189ad4330ef78d49edcdb87
2014874201 <1152579039@qq.com>
2018-06-08 11:12:31 +0800

    框架搭好了

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -36,6 +36,11 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'src': resolve('src'),
+      'common': resolve('src/common'),
+      'components': resolve('src/components'),
+      'base': resolve('src/base'),
+      'api': resolve('src/api')
     }
   },
   module: {
