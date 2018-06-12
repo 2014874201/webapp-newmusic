86c608cb9c67bf3e7bb47ec032ed90a83b91ad4d
2014874201 <1152579039@qq.com>
2018-06-12 15:36:17 +0800

    头部

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div id="app">
-    <router-view/>
+    <m-header></m-header>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    MHeader
+  }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import 'common/scss/index.scss';
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
 }
 </style>
--- src/components/m-header/m-header.vue
+++ src/components/m-header/m-header.vue
@@ -1,9 +1,61 @@
 <template>
-  <div></div>
+  <div class="m-header">
+    <h1 class="text">VMUSICJIN</h1>
+    <router-link to="/user" class="mine" tag="div">
+     <i class="iconfont icon-list"></i>
+    </router-link>
+    <router-link to="/search" class="search" tag="div">
+      <i class="iconfont icon-search"></i>
+    </router-link>
+  </div>
 </template>
+
 <script>
 export default {
+
 }
 </script>
+
 <style lang="scss" scoped>
+@import "~common/scss/variable.scss";
+
+.m-header {
+  position: relative;
+  height: 100px;
+  width: 100%;
+  text-align: center;
+  background: $color-theme;
+  .text {
+    line-height: 100px;
+    font-weight: bold;
+    color: $color-text-l;
+    letter-spacing: 3px;
+    font-size: 44px;
+  }
+  .mine {
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    .iconfont {
+      line-height: 100px;
+      display: block;
+      padding-left: 20px;
+      font-size: 44px;
+      color: $color-theme-l;
+    }
+  }
+  .search {
+    position: absolute;
+    top: 0;
+    right: 4px;
+    .iconfont {
+      display: block;
+      line-height: 100px;
+      padding-right: 20px;
+      font-size: 36px;
+      color: $color-theme-l;
+    }
+  }
+}
 </style>
