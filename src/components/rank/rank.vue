4c708339c0681c7712f0f22a1328d6262257fc5d
2014874201 <1152579039@qq.com>
2018-06-14 13:40:06 +0800

    排行榜

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -1,9 +1,113 @@
 <template>
-  <div></div>
+  <div class="rank" ref="rank">
+    <scroll class="toplist" ref="scroll" :data="TopList">
+      <ul>
+        <li class="item"  v-for="item in TopList" :key="item.id" @click="selectItem(item)">
+          <div class="icon">
+            <img v-lazy="item.coverImgUrl" width="100" height="100">
+          </div>
+          <ul class="songlist">
+            <li class="song" v-for="(song, index) in item.top" :key="index">
+              <span>{{index + 1}}.</span>
+              <span>{{song.name}} - {{song.ar[0].name}}</span>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <div v-show="showLoading" class="loading-content">
+        <loading></loading>
+      </div>
+    </scroll>
+  </div>
 </template>
 <script>
+import {getTop} from 'api/rank'
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+// import {playlistMixin} from 'common/js/mixin'
+
+const MUSIC_TOP = []
 export default {
+  data () {
+    return {
+      TopList: [],
+      showLoading: true
+    }
+  },
+  components: {
+    Scroll,
+    Loading
+  },
+  created () {
+    this.idx()
+    this._getTopList()
+  },
+  methods: {
+    idx () {
+      var i = 0
+      do {
+        MUSIC_TOP.push(i++)
+      } while (i <= 23)
+    },
+    _getTopList () {
+      for (let i = 0; i < MUSIC_TOP.length; i++) { // 重复请求
+        getTop(MUSIC_TOP[i]).then((res) => {
+          let list = res.data.playlist
+          list.top = res.data.playlist.tracks.slice(0, 3)
+          this.TopList.push(list)
+        })
+        if (i === MUSIC_TOP.length - 1) {
+          this.showLoading = false // 请求完毕
+        }
+      }
+      console.log('排行榜', this.TopList)
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
+@import "~common/scss/variable";
+@import "~common/scss/mixin";
+.rank {
+  position: fixed;
+  width: 100;
+  top: 200px;
+  bottom: 0;
+  .toplist {
+    height: 100%;
+    overflow: hidden;
+    padding-top: 5px;
+  }
+  .item {
+    display: flex;
+    margin:0 auto;
+    padding: 10px 0;
+    // height: 100px;
+    border-bottom: 1px solid rgb(228, 228, 228);
+    &:last-child {
+        padding-bottom: 20px;
+    }
+    .icon {
+      img {
+        border-radius: 10px;
+      }
+    }
+    .songlist{
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      padding:10px 20px;
+      font-size: $font-size-small-ss;
+      background: pink;
+      .song {
+        flex: 1;
+        @include no-wrap();
+        line-height: 30px;
+      }
+    }
+  }
+
+}
+
 </style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="recommend" ref="recommend">
-    <scroll class="recommend-content" ref="scroll">
+    <scroll class="recommend-content" ref="scroll" :data="playList">
       <!-- scroll只对第一个子元素起作用 -->
       <div>
         <div class="decorate" v-if="banner.length"></div>
@@ -13,6 +13,25 @@
           </slider>
         </div>
         <!-- /轮播图 -->
+        <!-- 歌单推荐 -->
+        <div class="recommend-list" ref="recommendList">
+          <h1 class="title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="item in playList" :key="item.id">
+              <div class="icon">
+                <img width="100" height="100" v-lazy="item.picUrl">
+              </div>
+              <p class="play-count">
+                <i class="fa fa-headphones"></i>
+                {{Math.floor(item.playCount / 10000) }}万
+              </p>
+              <div class="text">
+                <h2 class="name" v-html="item.name"></h2>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <!-- /歌单推荐 -->
       </div>
     </scroll>
   </div>
@@ -20,7 +39,7 @@
 <script>
 import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
-import {getBanner} from 'api/recommend'
+import {getBanner, getRecommendList} from 'api/recommend'
 import {ERR_OK} from 'common/js/config'
 export default {
   data () {
@@ -36,7 +55,7 @@ export default {
   },
   created () {
     this._getBanner()
-    // this._getRecommendList()
+    this._getRecommendList()
     // this._getRecommendMusic()
   },
   methods: {
@@ -47,6 +66,16 @@ export default {
           console.log(this.banner)
         } else {
           console.error('Banner 获取失败')
+        }
+      })
+    },
+    _getRecommendList () {
+      getRecommendList().then(res => {
+        if (res.status === ERR_OK) {
+          // console.log('热门推荐歌单', res)
+          this.playList = res.data.result
+        } else {
+          console.error('getRecommendList 获取失败')
         }
       })
     }
@@ -62,6 +91,7 @@ export default {
   top: 200px;
   bottom: 0;
   z-index: 100;
+  // background:#fff;
   .recommend-content {
     width: 100%;
     height: 100%;
@@ -69,17 +99,61 @@ export default {
     .decorate {
       position: absolute;
       width: 100%;
-      height: 20vh;
-      // top: -30vh; // 相对于视口的高度。视口被均分为100单位的vh
-      // z-index: -10;
+      height: 50vh;
+      top: -30vh; // 相对于视口的高度。视口被均分为100单位的vh
+      z-index: -10;
       background: $color-theme;
-      // vertical-align: inherit;
+      vertical-align: inherit;
     }
     .slider-wrapper {
       width: 95%;
       margin: 0 auto;
       border-radius: 5px;
       overflow: hidden;
+    }
+    .recommend-list {
+      position: relative;
+      box-sizing: border-box;
+      width: 100%;
+      text-align: center;
+      // .desc-title {
+      //   // margin-top:10px;
+      //   height: 50px;
+      //   width: 1%;
+      //   background:pink;
+      // }
+      .title {
+        padding-left:1.5%;
+        height: 65px;
+        line-height: 65px;
+        text-align: left;
+        font-size: $font-size-medium;
+        font-weight: bold;
+        color: $color-text;
+      }
+      .item {
+        display: flex;
+        margin: 20px;
+        border-radius: 10px;
+        .play-count {
+          position: absolute;
+          color: #fff;
+          font-weight: bold;
+          padding: 10px;
+        }
+        .text {
+          background: linear-gradient(rgba(109, 109, 109, 0.4),rgba(255, 255, 255, 0));
+          width: 100%;
+          margin-left: 20px;
+          .name {
+            height: 80px;
+            line-height: 80px;
+            padding: 20px;
+            font-size: $font-size-small-x;
+            text-align: left;
+          }
+        }
+      }
     }
   }
 }
